ba4340d8af659991aa0e9148a1f4d58e30c021cf
GeromGG <ty.gerom@gmail.com>
2021-04-18 01:50:59 +0300

    added system info providers

---
+++ src/Orc.SystemInfo/Services/Providers/Win32OperatingSystemSystemInfoProvider.cs
@@ -0,0 +1,26 @@
+﻿namespace Orc.SystemInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Catel.Logging;
+    using Catel.Services;
+
+    public class Win32OperatingSystemSystemInfoProvider : ISystemInfoProvider
+    {
+
+        private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+
+        private readonly ILanguageService _languageService;
+
+        public Win32OperatingSystemSystemInfoProvider(ILanguageService languageService)
+        {
+            _languageService = languageService;
+        }
+
+        public IEnumerable<SystemInfoElement> GetSystemInfoElements()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Orc.SystemInfo/Services/Providers/Win32ProcesorSystemInfoProvider.cs
@@ -0,0 +1,24 @@
+﻿namespace Orc.SystemInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Catel.Logging;
+    using Catel.Services;
+
+    public class Win32ProcesorSystemInfoProvider : ISystemInfoProvider
+    {
+        private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+
+        private readonly ILanguageService _languageService;
+
+        public Win32ProcesorSystemInfoProvider(ILanguageService languageService)
+        {
+            _languageService = languageService;
+        }
+        public IEnumerable<SystemInfoElement> GetSystemInfoElements()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
